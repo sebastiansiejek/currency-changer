baeeed4327a8ffd690012ad2578af3038581e563
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-02-14 21:26:25 +0100

    feat: add query params for latest exchange

--- src/repositories/exchange/ExchangeRepository.ts
+++ src/repositories/exchange/ExchangeRepository.ts
@@ -1,7 +1,10 @@
+import queryString from 'query-string'
+import { ILatestParams } from './IExchangeRepository'
 export default class ExchangeRepository {
   static api_url = 'https://api.exchangeratesapi.io'
 
-  static async getLatest() {
-    return await fetch(this.api_url + '/latest').then(res => res.json())
+  static async getLatest(params?: ILatestParams) {
+    const queryParams = params && `?${queryString.stringify(params)}`
+    return await fetch(this.api_url + `/latest${queryParams}`).then(res => res.json())
   }
 }
