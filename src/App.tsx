4fd889030e0a61aa6ae52734241fc484dbfd1f8c
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-02-16 20:09:23 +0100

    feat: add Input number for currency

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import 'antd/dist/antd.css'
 import Main from 'components/organisms/Main'
 import MainTemplate from 'templates/MainTemplate'
 import React from 'react'
--- src/components/organisms/Main/Main.tsx
+++ src/components/organisms/Main/Main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import CurrencyList from 'components/containers/CurrencyList'
+import { InputNumber } from 'antd'
 
 const Main = () => (
   <main>
     <h2>Main</h2>
+    <InputNumber min={0} step={1} />
     <CurrencyList />
   </main>
 )
