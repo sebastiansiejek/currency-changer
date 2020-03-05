441eeb28807189077513bde581417d50181f41ac
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-03-05 22:27:50 +0100

    feat: dispatch fetchLatestRates

--- src/repositories/exchange/ExchangeRepository.ts
+++ src/repositories/exchange/ExchangeRepository.ts
@@ -1,10 +1,17 @@
+import exchangeSlice from 'store/slices/exchangeSlice'
 import queryString from 'query-string'
+import store from 'store/store'
 import { ILatestParams } from './IExchangeRepository'
 export default class ExchangeRepository {
   static api_url = 'https://api.exchangeratesapi.io'
 
   static async getLatest(params?: ILatestParams) {
+    store.dispatch(exchangeSlice.actions.fetchLatestRates({ payload: true }))
     const queryParams = params ? `?${queryString.stringify(params)}` : ''
-    return await fetch(this.api_url + `/latest${queryParams}`).then(res => res.json())
+
+    return await fetch(this.api_url + `/latest${queryParams}`).then(res => {
+      store.dispatch(exchangeSlice.actions.fetchLatestRates({ payload: false }))
+      res.json()
+    })
   }
 }
