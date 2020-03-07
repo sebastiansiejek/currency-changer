6a125c3cb4a5b3ff4b54723d898fd966d18ec9f8
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-03-07 19:13:40 +0100

    feat: display latest ratets in table

--- src/components/containers/CurrencyList/CurrencyList.tsx
+++ src/components/containers/CurrencyList/CurrencyList.tsx
@@ -1,25 +1,48 @@
 import React from 'react'
-import { Row, Col } from 'antd'
+import { Table } from 'antd'
 import { connect } from 'react-redux'
 import { ExchangeReducerType } from 'store/types/reducerTypes'
 import { Spin } from 'antd'
 import { ExchangeStoreType } from 'store/types/storeTypes'
+import { currencyFormat } from 'utils/utils'
 
-const CurrencyList = ({ isFetchLatestRates }: ExchangeStoreType) => {
-  if (isFetchLatestRates) {
+const CurrencyList = ({ isFetchLatestRates, latestRates }: ExchangeStoreType) => {
+  if (isFetchLatestRates && latestRates?.base) {
+    const data: { key: string; currency: string; value: string }[] = []
+
+    for (let [key, value] of Object.entries(latestRates.rates)) {
+      data.push({
+        key,
+        currency: key,
+        value: currencyFormat(value, key)
+      })
+    }
+
     return (
-      <Row>
-        <Col>
-          <h2>test</h2>
-        </Col>
-      </Row>
+      <Table
+        pagination={false}
+        dataSource={data}
+        columns={[
+          {
+            title: 'Currency',
+            key: 'currency',
+            dataIndex: 'currency'
+          },
+          {
+            title: 'Value',
+            key: 'value',
+            dataIndex: 'value'
+          }
+        ]}
+      ></Table>
     )
   }
   return <Spin />
 }
 
 const mapStateToProps = ({ exchange }: ExchangeReducerType) => ({
-  isFetchLatestRates: exchange.isFetchLatestRates
+  isFetchLatestRates: exchange.isFetchLatestRates,
+  latestRates: exchange.latestRates
 })
 
 export default connect(mapStateToProps)(CurrencyList)
